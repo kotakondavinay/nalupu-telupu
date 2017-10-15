972edd584c0a14c75698478c836642db0ee1635e
Kotakonda Vinay Kumar <ramvinaynaidu@gmail.com>
2017-10-15 20:20:12 +0530

    Graham Scan and AB

---
+++ simple-problems-solutions/src/main/java/com/naltel/app/arrays/GrahamScan.java
@@ -0,0 +1,16 @@
+package com.naltel.app.arrays;
+
+public class GrahamScan {
+    static class Point {
+        int x;
+        int y;
+    }
+    public void grahamScan(int[][] a) {
+
+    }
+    public static void main(String[] args) {
+        int points[][] = new int[][]{{0, 3}, {1, 1}, {2, 2}, {4, 4},
+                {0, 0}, {1, 2}, {3, 1}, {3, 3}};
+
+    }
+}
